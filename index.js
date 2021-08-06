00459d0f4a35112c2c45d618f8a72df77a0bf6b0
Tobyenglish <tobyenglishD1@gmail.com>
2021-08-06 07:49:56 -0700

    basics

---
+++ index.js
@@ -0,0 +1,19 @@
+// build a function that mimics a roulette game.
+    // be able to bet on any number 1-36
+    // set the amount you want on any number
+// player starts with a cetain amount of money
+    //  if they win, they get money
+        // they win $36 for every $1
+    //  if they lose, they lose money
+// if they run out of money they need to go away
+// set the amount of money they play each hand
+// cash out or lose
+
+function roulette(choosenNumber){
+    const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35]
+
+    if (choosenNumber === any ){
+        return
+    }
+}
+
