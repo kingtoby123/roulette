616ce2193b30088af0cb95e4c24bc212476f5b22
Tobyenglish <tobyenglishD1@gmail.com>
2021-08-08 16:10:21 -0700

    added the random number feature

--- index.js
+++ index.js
@@ -9,11 +9,10 @@
 // set the amount of money they play each hand
 // cash out or lose
 
-function roulette(choosenNumber){
-    const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35]
-
-    if (choosenNumber === any ){
-        return
-    }
+function roulette(){
+    const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36]
+    randomNumber = []
+    random = numbers[Math.floor(Math.random()* numbers.length)]
+    randomNumber += random 
+    return randomNumber
 }
-
