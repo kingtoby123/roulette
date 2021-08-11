08e684393bb07fce16df768aae7a72f1bfd86bed
Tobyenglish <tobyenglishD1@gmail.com>
2021-08-10 19:57:53 -0700

    new format

--- index.js
+++ index.js
@@ -10,26 +10,29 @@
 // cash out or lose
 
 
-const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36]
-const wallet = []
-let total = 0 
+const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35]
+let wallet = 100
+let bets = []
 
+// indent properly
 function roulette(){
-randomNumber = []
 random = numbers[Math.floor(Math.random()* numbers.length)]
-randomNumber += random 
-return randomNumber
+return random
 }
-function choosenNumber (number, amount) {
-let choosenNumber 
-while (!choosenNumber ) {
-let userInput = prompt(`\nWhat ${amount} would you like to bet?\n`)
-let userInputTwo = prompt(`\nWhat ${number} would you like to bet on?\n`)
-choosenNumber  = numbers[number][userInput]
-if (!choosenNumber) {
+
+
+function bet (number) {
+let bet 
+while (!bet ) {
+let numberInput = prompt(`\nWhat ${number} would you like to bet on?\n`)
+numberInput = bets[numbers]
+if (!bet) {
   console.log("that number is not valid, please try again.")
 }
 }
+
+bets = numberInput
+
 }
 
 function combine(){
@@ -38,19 +41,9 @@ if(choosenNumber === roulette){
 }
 }
 
-//   console.log(`The ${orderedItem.name} is a great choice! That will be $${orderedItem.price}`)
 
-//   orderedItems.push(orderedItem.name)
-//   bill += parseFloat(orderedItem.price)
-// }
+console.log("Welcome to the roulette table! try your luck and win big!")
+console.log(`\nhere is your wallet, spend your money wrecklessly! your total is $${wallet}`)
+console.log("\nPick a number between 1 and 35")
+bet("number")
 
-
-
-// function matching(){
-//     if(choosenNumber === randomNumber){
-//         console.log("congrats!! you won!!")
-//     }
-// }
-console.log("Welcome to the roulette table! please give me your money")
-console.log(choosenNumber("number", "amount"))
-console.log(choosenNumber("number", "amount"))
