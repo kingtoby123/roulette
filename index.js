955df26a691480865cc115adf32574fd41d8b9c7
Tobyenglish <tobyenglishD1@gmail.com>
2021-08-16 08:06:09 -0700

    A lot

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
-// build a function that mimics a roulette game.
-//     be able to bet on any number 1-35
-//     set the amount you want on any number
-// player starts with a cetain amount of money
-//      if they win, they get money
-//         they win $36 for every $1
-//      if they lose, they lose money
-// if they run out of money they need to go away
-// set the amount of money they play each hand
-// cash out or lose
-
-
 const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36]
 let wallet = 100
 let bets = []
-let myNumber = []
-let myAmount = []
+let myNumber = 0
+let myAmount = 0
+let myRandom = 0
+
+
 
 function roulette(){
     random = numbers[Math.floor(Math.random()* numbers.length)]
@@ -22,38 +13,48 @@ function roulette(){
 }
 
 function choosenNumber () {
-    let choosenNumber  
-    while (!choosenNumber) {
-        let numberInput = prompt(`\nPlease pick a number between the numbers from 1 and 36\n`)
-        myNumber = numberInput 
-        break
+    while (true) {
+        let numberInput = parseInt(prompt(`\npick any number from 1 & 36\n`))
+        if(numbers.includes(numberInput)){
+            myNumber = numberInput 
+            console.log("Great choice! that's my lucky number")
+            break
+        }else{
+            console.log("sorry try again")
+        }
     }
-
-    console.log("Great choice! that's my lucky number")
 }
+    
 
 function amount(){
-    let amount 
-    while(!amount){
-        let amountInput = prompt(`\nNow, please put the amount you want to bet!\n`)
-        myAmount = amountInput
+    while(true){
+        let amountInput = parseInt(prompt(`\nHow much do you want to wager? \n`))
+        myAmount = wallet  - amountInput
+        wallet = parseFloat(myAmount)
+        console.log("Wow! Big Spender")
         break
     }
-
-    console.log("Big Spender!")
 }
 
+function matching(){
+    randomNumber = roulette()
+    console.log(randomNumber)
+    if (myNumber === randomNumber){
+        console.log("Congrats!! You won, here is your prize money")
+        matchingAmount =  myAmount * 35
+        wallet += matchingAmount
+    } 
+    else{
+        console.log("Dang! Almost got it, Try again!")
+    }
+}
 
 
 console.log("Welcome to the roulette table! try your luck and win big!")
 console.log(`\nhere is your wallet, spend your money wrecklessly! your total is $${wallet}`)
-// betTwo("amount")
 choosenNumber()
 amount()
-console.log()
-
-
-
-
-
+console.log("We will now spin the Roulette Table! Did you manage to hit your lucky number?")
+matching()
+console.log(`here is what is remaining in your wallet $${myAmount}`)
 
